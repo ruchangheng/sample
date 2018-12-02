fd820806802dfed88894ec50813f573b82c9170c
ruchangheng <624813409@qq.com>
2018-12-02 17:48:19 +0000

    Finish user signup

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function create()
     {
     	return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+    	return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+    	$this->validate($request, [
+    		/*
+    			required:验证是否为空
+    			min:3|max:50 限制最小长度和最大长度
+    			多个条件时可以使用|分隔
+    			email：邮箱验证
+    			unique:user 验证user表某数据的唯一性
+    			confirmed：密码匹配，保证两次输入密码一致
+    		 */
+
+    		'name' => 'required|max:50',
+    		'email' => 'required|email|unique:users|max:255',
+        	'password' => 'required|confirmed|min:6'
+    	]);
+
+    	$user = User::create([
+    		'name' => $request->name,
+    		'email' => $request->email,
+            'password' => bcrypt($request->password),
+    	]);
+
+    	session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+    	return redirect()->route('users.show', [$user]);
+
     }
 }
