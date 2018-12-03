4152080dff7a890c11c23af06a9759813d182033
ruchangheng <624813409@qq.com>
2018-12-03 18:15:50 +0000

    Add user following

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,10 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 // 微博操作
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+// 关注列表和粉丝列表
+Route::get('/users/{user}/followings', 'UserController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UserController@followers')->name('users.followers');
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
