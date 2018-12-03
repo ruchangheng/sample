5acd855e847e1028e208eb3b1243ad5844917fc6
ruchangheng <624813409@qq.com>
2018-12-03 14:28:51 +0000

    Finish user edit, update, index and destroy actions

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,18 @@
 //     return view('welcome');
 // });
 
+// 静态页面路由
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+// 注册页路由
 Route::get('signup', 'UserController@create')->name('signup');
+
+// 用户信息路由
 Route::resource('users', 'UserController');
 
+// 登录退出路由
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
